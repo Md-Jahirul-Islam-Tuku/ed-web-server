194ab3f5f2554cb7487b5404c7c49991d3104fc8
Tuku Webian <tukuwebian@gmail.com>
2022-10-26 18:45:04 +0600

    courses API create

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const courses = require('./data/courses.json')
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('EdWeb server running');
+})
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
 })
 
 app.listen(port, () => {
